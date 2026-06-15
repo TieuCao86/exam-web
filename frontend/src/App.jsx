ca5447ca6fdf9cd645f02a249d08040c98f18bd7
TieuCao86 <caotrung862005@gmail.com>
2026-06-15 15:17:13 +0700

    add protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import MainLayout from './components/MainLayout';
-import Calendar from './pages/Calendar';
-import CoursePage from './pages/CoursePage';
+import MainLayout from "./components/MainLayout";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import Login from "./pages/Login";
+import Calendar from "./pages/Calendar";
+import CoursePage from "./pages/CoursePage";
+import Forbidden from "./pages/Forbidden";
 
 function App() {
 
     const mockMenus = [
-        { url: '/calendar', icon: 'fas fa-calendar-alt', text: 'Lịch học' },
-        { url: '/courses', icon: 'fas fa-book', text: 'Khóa học' },
+        { url: "/calendar", icon: "fas fa-calendar-alt", text: "Lịch học" },
+        { url: "/courses", icon: "fas fa-book", text: "Khóa học" }
     ];
 
     return (
         <Routes>
+
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/403" element={<Forbidden />} />
 
             <Route
                 path="/"
@@ -21,18 +29,22 @@ function App() {
             <Route
                 path="/calendar"
                 element={
-                    <MainLayout menus={mockMenus} currentPath="/calendar">
-                        <Calendar />
-                    </MainLayout>
+                    <ProtectedRoute allowedRoles={["STUDENT"]}>
+                        <MainLayout menus={mockMenus} currentPath="/calendar">
+                            <Calendar />
+                        </MainLayout>
+                    </ProtectedRoute>
                 }
             />
 
             <Route
                 path="/courses"
                 element={
-                    <MainLayout menus={mockMenus} currentPath="/courses">
-                        <CoursePage />
-                    </MainLayout>
+                    <ProtectedRoute allowedRoles={["STUDENT"]}>
+                        <MainLayout menus={mockMenus} currentPath="/courses">
+                            <CoursePage />
+                        </MainLayout>
+                    </ProtectedRoute>
                 }
             />
 
